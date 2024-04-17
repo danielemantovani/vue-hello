13b57788fd715eaa4a832b94fd103c9d3d8c9565
danielemantovani <daniele.mantovani124@gmail.com>
2024-04-17 19:01:15 +0200

    feat: hello VueJs completed

--- js/script.js
+++ js/script.js
@@ -4,11 +4,25 @@ createApp ({
     data () {
         return {
             greeting: "ACCESSO NON AUTORIZZATO!!! CHIUDI IMMEDIATAMENTE LA PAGINA!!!!!",
-            istruction: "MA SOPRATTUTTO NON PREMERE SU QUESTO PULSANTE O DOVRAI VEDERTELA CON LUI.....",
-            question: "Se proprio vuoi proseguire e accettare il rischio prova a indovinare di chi sto parlando"
+            istruction: "MA SOPRATTUTTO NON PREMERE SUL PULSANTE QUI SOTTO O DOVRAI VEDERTELA CON LUI.....",
+            question: "Se proprio vuoi proseguire e accettare il rischio prova a indovinare di chi sto parlando",
+            guessWho: "",
+            wrong: "",
+            colorText: "red-text",
+            image: "imgWTR.jpg",
+            showImage: false,
         }
     },
     methods: {
+        // hereItIs: function(){
+        //     this.wrong = "TI AVEVO AVVISATO DI NON FARLO"
+        //     this.guessWho = ""
+        // },
+        toggleImage: function() {
+            this.showImage = !this.showImage;
+            this.wrong = "SBAGLIATO, TI AVEVO AVVISATO DI NON FARLO";
+            this.guessWho = "";
+        }
 
     }
 
